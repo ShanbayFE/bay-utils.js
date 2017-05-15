c46fd5c1fc0c9819a2c33f7fbfd859468e4ef9e2
wenlong guo <wenlong.guo@shanbay.com>
2017-05-15 17:51:43 +0800

    add wx jssdk config func

---
+++ src/wxSdkConfig.js
@@ -0,0 +1,69 @@
+import { isWechatUA } from './validator';
+import { ajax } from './others';
+
+const SHANBAY_PREFIX_V1 = 'https://www.shanbay.com/api/v1';
+const LOCAL_PREFIX_V1 = 'http://local.daydayup.me:3000/api/v1';
+
+/*
+默认的JS接口列表：
+    onMenuShareTimeline: 分享到朋友圈,
+    onMenuShareAppMessage: 分享给朋友,
+    onMenuShareQQ: 分享到QQ,
+    onMenuShareQZone: 分享到QQ空间,
+*/
+
+const defaultJsApiList = [
+    'onMenuShareTimeline',
+    'onMenuShareAppMessage',
+    'onMenuShareQQ',
+    'onMenuShareQZone',
+];
+
+/*
+参数说明：
+    shareData: Object, 用以分享的信息，{title, link, imgUrl, success, cancel}
+    jsApiList: Array, 需要使用的JS接口列表,
+    onReady: 成功验证后的回调函数,
+    isDev: 是否为开发环境，默认为false,
+    isDebug: 是否开启调试模式
+*/
+
+export const wxSdkConfig = (param) => {
+    const { shareData,
+            jsApiList = defaultJsApiList,
+            onReady,
+            isDev = false,
+            isDebug = false,
+        } = param;
+    if (isWechatUA(window.navigator.userAgent)) {
+        ajax({
+            url: `${isDev ? LOCAL_PREFIX_V1 : SHANBAY_PREFIX_V1}/wechat/jsconfig/?url=${window.location.href}`,
+            success: (data) => {
+                const config = {
+                    debug: isDebug,
+                    appId: data.appid,
+                    timestamp: data.timestamp,
+                    nonceStr: data.noncestr,
+                    signature: data.signature,
+                    jsApiList,
+                };
+                wx.config(config);
+                wx.ready(() => {
+                    ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone'].forEach((jsApi) => {
+                        if (shareData && jsApiList.indexOf(jsApi) !== -1) {
+                            wx[jsApi](shareData);
+                        }
+                    });
+                    onReady && onReady({
+                        appId: data.appid,
+                        timestamp: data.timestamp,
+                        nonceStr: data.noncestr,
+                        signature: data.signature,
+                    });
+                });
+            },
+        });
+    } else {
+        onReady && onReady();
+    }
+};
