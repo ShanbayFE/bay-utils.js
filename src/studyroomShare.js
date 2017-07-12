94feb1c9aae8a7096e2932977466d2022e571a69
JasonSi <pujiaxun@gmail.com>
2017-07-12 17:43:26 +0800

    fix

--- src/studyroomShare.js
+++ src/studyroomShare.js
@@ -1,34 +1,32 @@
 import { ajax } from './others';
 
-export const studyroomShare = (
-    userContent,
-    shareContent,
-    onSuccess = () => {},
-    onError = () => {},
-) => {
+export const studyroomShare = (param) => {
     const SHANBAY_PREFIX_V2 = 'https://www.shanbay.com/api/v2';
+    const LOCAL_PREFIX_V2 = 'http://www.daydayup.me/api/v2';
+    const SHANBAY_LOGO_URL = 'https://static.baydn.com/baydn/public/images/logo-s.png';
 
     const {
-        title,
-        content,
-        image_url,
-        redirect_url,
-    } = shareContent;
+        data,
+        isDev = false,
+        onSuccess,
+        onError,
+    } = param;
 
-    const data = {
-        content: userContent,
-        share_content: {
-            title,
-            content,
-            image_url,
-            redirect_url,
-        },
+    const shareContent = {
+        title: document.title,
+        image_url: SHANBAY_LOGO_URL,
+        redirect_url: window.location.href,
     };
 
+    Object.assign(shareContent, data.share_content);
+
     ajax({
-        url: `${SHANBAY_PREFIX_V2}/studyroom/posts/share/`,
+        url: `${isDev ? LOCAL_PREFIX_V2 : SHANBAY_PREFIX_V2}/studyroom/posts/share/`,
         type: 'POST',
-        data,
+        data: {
+            content: data.content,
+            share_content: shareContent,
+        },
         success: onSuccess,
         error: onError,
     });
